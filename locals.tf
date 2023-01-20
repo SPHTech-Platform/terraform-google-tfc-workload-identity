8f16c4010e3301aed343b880952416599a9d4cd5
Yong Wen Chua <lawliet89@users.noreply.github.com>
2023-01-20 11:26:27 +0800

    Truncate workload identity pool IDs

--- locals.tf
+++ locals.tf
@@ -1,7 +1,9 @@
 locals {
-  workspaces = merge(
+  flattened_workspaces = merge(
     [for org, workspaces in var.workspaces : { for ws in workspaces : "${org}-${ws}" => { org = org, ws = ws } }]...
   )
+
+  workspaces = { for k, ws in local.flattened_workspaces : length(k) > 32 ? join("-", [substr(k, 0, 23), substr(sha512(k), 0, 8)]) : k => ws }
 
   # See https://cloud.google.com/iam/docs/workload-identity-federation#impersonation
   pool_principal_base = "principalSet://iam.googleapis.com/projects/${data.google_project.this.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.tfc.workload_identity_pool_id}"
